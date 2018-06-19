59fea0aa084ce98f1b6abef5d433a15f0659f96e
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2018-06-19 13:56:47 -0300

    Add Working solar system

--- solar_system/main.cpp
+++ solar_system/main.cpp
@@ -15,14 +15,27 @@ class IdleHandler : public VART::ViewerGlutOGL::IdleHandler {
     VART::Transform* moonSelfAxisRotation = new VART::Transform();
     VART::Transform* sunSelfAxisRotation = new VART::Transform();
 
+    VART::Transform* earthRotationAroundSun = new VART::Transform();
+    VART::Transform* moonRotationAroundEarth = new VART::Transform();
+
+
     virtual void OnIdle(){
-      earthSelfAxisRotationRadians += 0.01;
-      moonSelfAxisRotationRadians += 0.01;
+      earthSelfAxisRotationRadians += 0.02;
+      moonSelfAxisRotationRadians += 3;
       sunSelfAxisRotationRadians += 0.01;
 
       earthSelfAxisRotation->MakeYRotation(earthSelfAxisRotationRadians);
       moonSelfAxisRotation->MakeYRotation(moonSelfAxisRotationRadians);
       sunSelfAxisRotation->MakeYRotation(sunSelfAxisRotationRadians);
+
+      earthRotationAroundSunRadians += 0.01;
+      earthRotationAroundSun->MakeRotation(VART::Point4D(0,0,0), VART::Point4D::Y(), earthRotationAroundSunRadians);
+
+      moonRotationAroundEarthRadians += 0.01;
+      moonRotationAroundEarth->MakeRotation(VART::Point4D(0,0,0), VART::Point4D::Y(), moonRotationAroundEarthRadians);
+
+
+      
 
       viewerPtr->PostRedisplay();
 
@@ -33,9 +46,11 @@ class IdleHandler : public VART::ViewerGlutOGL::IdleHandler {
     double moonSelfAxisRotationRadians;
     double sunSelfAxisRotationRadians;
 
-
-
+    double earthRotationAroundSunRadians;
+    double moonRotationAroundEarthRadians;
 };
+
+// 1)Fazer girar a lua em torno da Terra
 
 int main(int argc, char* argv[]) {
   VART::ViewerGlutOGL::Init(&argc, argv);
@@ -53,13 +68,15 @@ int main(int argc, char* argv[]) {
   VART::Transform sunScale;
   VART::Transform sunTranslation;
   VART::Transform sunSelfRotation;
-  VART::Transform sunRotationAroundSun;
 
   VART::Sphere moon;
+  
   VART::Transform moonScale;
   VART::Transform moonTranslation;
   VART::Transform moonSelfRotation;
-  VART::Transform moonRotationAroundSun;
+  VART::Transform moonRotationAroundEarth;
+
+  VART::Transform moonAndEarthTranslation;
 
   VART::Texture texture;
   VART::Material material;
@@ -68,12 +85,13 @@ int main(int argc, char* argv[]) {
     material.SetTexture(texture);
     moon.SetMaterial(material);
     
-    moonScale.MakeScale(1,1,1);
-    moonTranslation.MakeTranslation(16,0,0);
+    // moonScale.MakeScale(1,1,1);
+    // moonTranslation.MakeTranslation(16,0,0);
 
-    moonScale.AddChild(moon);
-    moonSelfRotation.AddChild(moonScale);
-    moonTranslation.AddChild(moonSelfRotation);
+    // moonScale.AddChild(moon);
+    // moonSelfRotation.AddChild(moonScale);
+    // moonTranslation.AddChild(moonSelfRotation);
+    // moonSelfRotation.AddChild(moonScale);
 
   } else {
     cerr << "missing moon texture" << endl;
@@ -84,13 +102,14 @@ int main(int argc, char* argv[]) {
     material.SetTexture(texture);
     earth.SetMaterial(material);
     
-    earthScale.MakeScale(2,2,2);
-    earthTranslation.MakeTranslation(12,0,0);
+    // earthScale.MakeScale(2,2,2);
+    // earthTranslation.MakeTranslation(12,0,0);
 
 
-    earthScale.AddChild(earth);
-    earthSelfRotation.AddChild(earthScale);
-    earthTranslation.AddChild(earthSelfRotation);
+    // earthScale.AddChild(earth);
+    // earthSelfRotation.AddChild(earthScale);
+    // earthTranslation.AddChild(earthSelfRotation);
+    // earthRotationAroundSun.AddChild(earthTranslation);
     
   } else {
     cerr << "missing earth texture" << endl;
@@ -101,26 +120,58 @@ int main(int argc, char* argv[]) {
     material.SetTexture(texture);
     sun.SetMaterial(material);
     
-    sunScale.MakeScale(4,4,4);
-    sunTranslation.MakeTranslation(0,0,0);
+    // sunScale.MakeScale(4,4,4);
+    // sunTranslation.MakeTranslation(0,0,0);
 
-    sunScale.AddChild(sun);
-    sunSelfRotation.AddChild(sunScale);
-    sunTranslation.AddChild(sunSelfRotation);
+    // sunScale.AddChild(sun);
+    // sunSelfRotation.AddChild(sunScale);
+    // sunTranslation.AddChild(sunSelfRotation);
 
   } else {
     cerr << "missing sun texture" << endl;
     return 0;
   }
 
-  scene.AddObject(&moonTranslation);
-  scene.AddObject(&earthTranslation);
-  scene.AddObject(&sunTranslation);
+  earthScale.MakeScale(2,2,2);
+  
+  moonScale.MakeScale(1,1,1);
+  moonTranslation.MakeTranslation(6,0,0);
+
+  moonAndEarthTranslation.MakeTranslation(16,0,0);
+
+  sunScale.MakeScale(6,6,6);
+  sunTranslation.MakeTranslation(0,0,0);
+
+
+
+  earthScale.AddChild(earth);
+  earthSelfRotation.AddChild(earthScale);
+  
+  moonScale.AddChild(moon);
+  moonSelfRotation.AddChild(moonScale);
+  moonTranslation.AddChild(moonSelfRotation);
+  moonRotationAroundEarth.AddChild(moonTranslation);
+
+  sunScale.AddChild(sun);
+  sunTranslation.AddChild(sunScale);
+  sunSelfRotation.AddChild(sunTranslation);
+
+  moonAndEarthTranslation.AddChild(moonRotationAroundEarth);
+  moonAndEarthTranslation.AddChild(earthSelfRotation);
+
+  earthRotationAroundSun.AddChild(moonAndEarthTranslation);
+  earthRotationAroundSun.AddChild(sunSelfRotation);
+  
+  scene.AddObject(&earthRotationAroundSun);
+
 
   IdleHandler idleHandler;
   idleHandler.sunSelfAxisRotation = &sunSelfRotation;
   idleHandler.moonSelfAxisRotation = &moonSelfRotation;
   idleHandler.earthSelfAxisRotation = &earthSelfRotation;
+
+  idleHandler.earthRotationAroundSun = &earthRotationAroundSun;
+  idleHandler.moonRotationAroundEarth = &moonRotationAroundEarth;
 
   // Create some objects
   VART::Arrow arrowX(2);
